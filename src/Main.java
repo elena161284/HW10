76d57cb99ad80330ae8a68d2c26380bcc355ec34
Elena <elena161284@mail.ru>
2023-05-21 20:08:36 +0300

    HW10.1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         task1();
-
+        task2();
+        task3();
     }
 
     public static void task1() {
         System.out.println("Задача 1");
+        String lastName="Ivanov";
+        String firstName="Ivan";
+        String middleName="Ivanovich";
+        String fullName = lastName+ " "+firstName+ " "+middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName="Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'e');
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+
     }
 }
